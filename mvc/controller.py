e2b178dd99185295f215c54b51d6bb5ae69bc39f
tehuano <rommelgh@gmail.com>
2020-02-07 15:29:26 -0600

    new

--- mvc/controller.py
+++ mvc/controller.py
@@ -20,16 +20,30 @@ class MyController():
         #initialize objects in view
         #a non cheat way to do MVC wiht tkinter control variables
         #self.view.setEntry_text('Data') 
-        self.view.setLabel_text('>')
-     #event handlers
+        #self.view.setLabel_text('>')
+    #event handlers
     def salirButtonPressed(self):
         self.parent.destroy()
     def calcularButtonPressed(self):
-        self.view.setLabel_text(self.view.entry_text.get())
-        #low_level_driver.usage('test')
+        key = self.view.getEntry_text_key()
+        msg =  self.view.getEntry_text_msg()
         self.model.clearList()
-        self.model.addToList('msg')
-        self.model.addToList('key')
+        if len(key):
+            self.model.addToList(key[0])
+        if len(msg):
+            self.model.addToList(msg)
+        if (len(key) and len(msg)):
+            self.view.setLabel_text(self.getCipherText(self.model.getList()))
     def listChangedDelegate(self):
         #model internally chages and needs to signal a change
         print(self.model.getList())
+    def getCipherText(self, in_list):
+        key = in_list[0]
+        msg = in_list[1]
+        values = []
+        print("Key = ", key)
+        for x in msg:
+            print("In: ", x)
+            values.append(chr(ord(key) ^ ord(x)))
+        print(values)
+        return ''.join(values)
--- mvc/view.py
+++ mvc/view.py
@@ -11,16 +11,17 @@ class MyView(Frame):
         welcome = Label(self.frame, text="XOR Cipher Interface")
         welcome.config(font=('Arial', 12))
         welcome.grid(row=0, padx = 30, pady = 5, columnspan = 3)
-        label_llave = Label(self.frame,textvariable = self.label_text_llave).grid(row = 1, padx = 10, sticky = W)
-        label_mensaje = Label(self.frame,textvariable = self.label_text_mensaje).grid(row = 2, padx = 10, sticky = W)
-        entry_llave = Entry(self.frame,textvariable = self.entry_text_llave).grid(row = 1,column = 1, columnspan = 4, sticky = W)
-        entry_mensaje = Entry(self.frame,textvariable = self.entry_text_mensaje).grid(row = 2, column = 1, columnspan = 4, sticky = W)
+        label_llave = Label(self.frame,textvariable = self.label_text_llave).grid(row = 1, padx = 10, pady = 5, sticky = W)
+        label_mensaje = Label(self.frame,textvariable = self.label_text_mensaje).grid(row = 2, padx = 10, pady =5, sticky = W)
+        entry_llave = Entry(self.frame,textvariable = self.entry_text_llave).grid(row = 1,column = 1, columnspan = 4, pady = 5, sticky = W)
+        entry_mensaje = Entry(self.frame,textvariable = self.entry_text_mensaje).grid(row = 2, column = 1, columnspan = 4, pady = 5, sticky = W)
         quitButton = Button(self.frame,text = 'Salir', command= self.vc.salirButtonPressed, highlightbackground='#3E4149').grid(row = 3, column = 2, pady = 5)
         addButton = Button(self.frame,text = "Calcular", command = self.vc.calcularButtonPressed, highlightbackground='#3E4149').grid(row = 3, column = 1, pady = 5)
-        label_resultado = Label(self.frame,textvariable = self.label_text_resultado).grid(row = 4, padx = 10, pady = 10, columnspan = 4, sticky = W)
+        label_title = Label(self.frame, text="Resultado:").grid(row = 4, padx = 10, pady = 8, sticky = W)
+        entry_resultado = Entry(self.frame,textvariable = self.label_text_resultado).grid(row = 4, column = 1, pady = 8, columnspa = 3, sticky = W)
 
     def __init__(self,vc):
-        vc.parent.geometry("300x150+150+200")
+        vc.parent.geometry("280x180+150+200")
         self.frame=Frame()
         self.frame.grid(row = 0,column=1)
         self.vc = vc
@@ -29,13 +30,16 @@ class MyView(Frame):
         self.entry_text_mensaje = StringVar()
         self.entry_text_mensaje.set('')
         self.label_text_llave = StringVar()
-        self.label_text_llave.set('LLave(hex)')
+        self.label_text_llave.set('LLave')
         self.label_text_mensaje = StringVar()
-        self.label_text_mensaje.set('Mensaje(hex)')
+        self.label_text_mensaje.set('Mensaje')
         self.label_text_resultado = StringVar()
         self.label_text_resultado.set('')
         self.loadView()
-    def getEntry_text(self):
+    def getEntry_text_key(self):
+    #returns a string of the entry text
+        return self.entry_text_llave.get()
+    def getEntry_text_msg(self):
     #returns a string of the entry text
         return self.entry_text_mensaje.get()
     def setEntry_text(self,text):
